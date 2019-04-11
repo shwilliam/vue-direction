10e2cbe08fcb678cd6bc6cab4070f9eb6d3afe14
William L <w-lindvall@outlook.com>
2019-04-11 13:02:33 -0700

    style: format

--- src/vue-direction.vue
+++ src/vue-direction.vue
@@ -1,44 +1,42 @@
 <script>
 export default {
-    name: 'VueDirection',
-    data() {
-        return {
-            xDirection: '',
-            yDirection: '',
-            oldX: 0,
-            oldY: 0,
-        }
-    },
-    computed: {
-        mouseDirection() {
-            return { x: this.xDirection, y: this.yDirection }
-        },
-    },
-    methods: {
-        getMouseDirection(e) {
-            if (this.oldX < e.pageX) {
-                this.xDirection = 'right'
-            } else {
-                this.xDirection = 'left'
-            }
-        
-            if (this.oldY < e.pageY) {
-                this.yDirection = 'down'
-            } else {
-                this.yDirection = 'up'
-            }
-        
-            this.oldX = e.pageX
-            this.oldY = e.pageY
-        },
-    },
-}
+  name: "VueDirection",
+  data() {
+    return {
+      xDirection: "",
+      yDirection: "",
+      prevPageX: 0,
+      prevPageY: 0
+    };
+  },
+  computed: {
+    mouseDirection() {
+      return { x: this.xDirection, y: this.yDirection };
+    }
+  },
+  methods: {
+    getMouseDirection(e) {
+      if (this.prevPageX < e.pageX) {
+        this.xDirection = "right";
+      } else {
+        this.xDirection = "left";
+      }
+
+      if (this.prevPageY < e.pageY) {
+        this.yDirection = "down";
+      } else {
+        this.yDirection = "up";
+      }
+
+      this.prevPageX = e.pageX;
+      this.prevPageY = e.pageY;
+    }
+  }
+};
 </script>
 
 <template>
-    <div @mousemove="getMouseDirection">
-        <slot
-            :mouseDirection="mouseDirection"
-        />
-    </div>
+  <div @mousemove="getMouseDirection">
+    <slot :mouseDirection="mouseDirection"/>
+  </div>
 </template>
